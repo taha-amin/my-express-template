01cf7c512cb08a606e8ce0a37fbf229d01a3d7d3
Mohamed Amin <mohamedamin2009ster@gmail.com>
2022-08-09 12:38:23 -0700

    wrote and passed test for deleting items for valid user

--- tests/items.test.js
+++ tests/items.test.js
@@ -105,4 +105,21 @@ describe('/api/v1/items', () => {
       message: 'You do not have access to view this page',
     });
   });
+
+  it('DELETE /:id should delete items for valid user', async () => {
+    const { agent } = await signUpUser();
+
+    const { body: item } = await agent.post('/api/v1/items').send({
+      description: 'apples',
+      qty: 6,
+    });
+
+    const { status, body } = await agent.delete(`/api/v1/items/${item.id}`);
+    expect(status).toBe(200);
+    expect(body).toEqual(item);
+
+    const { body: items } = await agent.get('/api/v1/items');
+
+    expect(items.length).toBe(0);
+  });
 });
