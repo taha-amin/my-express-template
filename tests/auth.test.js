7edeb5109c94c1950eb3e0d69373dd127306c30f
Mohamed Amin <mohamedamin2009ster@gmail.com>
2022-08-09 10:52:23 -0700

    wrote test for /signup

---
+++ tests/auth.test.js
@@ -0,0 +1,19 @@
+const { setupDb, signUpUser } = require('./utils.js');
+const request = require('supertest');
+const app = require('../lib/app');
+
+describe('/api/v1/auth', () => {
+  beforeEach(setupDb);
+
+  it('/signup', async () => {
+    const { agent, user, credentials } = await signUpUser();
+
+    expect(user).toEqual({
+      id: expect.any(String),
+      email: credentials.email,
+    });
+
+    const { statusCode } = await agent.get('/api/v1/auth/verify');
+    expect(statusCode).toBe(200);
+  });
+});
