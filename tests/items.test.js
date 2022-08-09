40ee126ade36ac86d1294aeb10b88a936cc55c92
Mohamed Amin <mohamedamin2009ster@gmail.com>
2022-08-09 12:20:27 -0700

    wrote and passed test for returning all items associated with the auth user

--- tests/items.test.js
+++ tests/items.test.js
@@ -5,7 +5,7 @@ const app = require('../lib/app');
 describe('/api/v1/items', () => {
   beforeEach(setupDb);
 
-  it.only('POST / creates a new shopping item with current user', async () => {
+  it('POST / creates a new shopping item with current user', async () => {
     const { agent, user } = await signUpUser();
 
     const newItem = { description: 'eggs', qty: 12 };
@@ -18,5 +18,32 @@ describe('/api/v1/items', () => {
       user_id: user.id,
       bought: false,
     });
+  });
+
+  it('GET / returns all items associated with the authenticated User', async () => {
+    // create a user
+    const { agent } = await signUpUser();
+    const { body: user1Item } = await agent.post('/api/v1/items').send({
+      description: 'apples',
+      qty: 6,
+    });
+
+    const { agent: agent2 } = await signUpUser({
+      email: 'user2@email.com',
+      password: 'password',
+    });
+
+    const { body: user2Item } = await agent2.post('/api/v1/items').send({
+      description: 'eggs',
+      qty: 12,
+    });
+
+    const resp1 = await agent.get('/api/v1/items');
+    expect(resp1.status).toEqual(200);
+    expect(resp1.body).toEqual([user1Item]);
+
+    const resp2 = await agent2.get('/api/v1/items');
+    expect(resp2.status).toEqual(200);
+    expect(resp2.body).toEqual([user2Item]);
   });
 });
